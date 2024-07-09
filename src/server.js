cc768224277a003920753ca416e5aec99e2610b0
KiraKostina <kostina.kira@gmail.com>
2024-07-10 00:04:31 +0300

    add init+server part 1

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+export const setupServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
